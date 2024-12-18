c9fedefac93ce69db2591584d38720a8695e34e1
deep3576 <deep3576@gmail.com>
2024-12-17 22:02:15 -0500

    update

--- internal/static/js/signup.js
+++ internal/static/js/signup.js
@@ -3,25 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const passwordField = document.querySelector('#password');
 
     togglePassword.addEventListener('click', () => {
-        // Toggle the type attribute
         const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
         passwordField.setAttribute('type', type);
 
-        // Toggle the eye / eye-slash icon
-        togglePassword.classList.toggle('fa-eye');
-        togglePassword.classList.toggle('fa-eye-slash');
+        // Toggle icon classes
+        if (type === 'text') {
+            togglePassword.classList.remove('fa-eye');
+            togglePassword.classList.add('fa-eye-slash');
+        } else {
+            togglePassword.classList.remove('fa-eye-slash');
+            togglePassword.classList.add('fa-eye');
+        }
     });
 });
-
-function validatePasswords() {
-    const pwd = document.getElementById('password').value;
-    const confirmPwd = document.getElementById('confirm_password').value;
-    const errorMessage = document.getElementById('error-message');
-
-    if (pwd !== confirmPwd) {
-        errorMessage.style.display = 'block';
-        return false; // Prevent form submission
-    }
-    errorMessage.style.display = 'none';
-    return true; // Allow form submission
-}
